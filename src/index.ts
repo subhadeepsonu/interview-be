2fe40cee32f882033db51ba2c24f41953f87d85a
subhadeepsonu <subhadeepthandaka@gmail.com>
2025-09-19 01:37:30 +0530

    added swagger  ui

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express from "express"
+import swaggerJSDoc from "swagger-jsdoc"
+import swaggerUi from "swagger-ui-express"
 import { userRouter } from "./routes/user.route"
 import { applicationRouter } from "./routes/application.route"
 import { jobPostsRouter } from "./routes/job-posts.route"
@@ -15,12 +17,45 @@ app.get("/", (req, res) => {
     })
 })
 
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "My Interview API",
+            version: "1.0.0",
+            description: "API documentation",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ["./src/routes/*.ts"],
+};
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/application", applicationRouter)
 app.use("/api/v1/job-posts", jobPostsRouter)
 app.use("/api/v1/interview", interviewRouter)
 app.use("/api/v1/recruiter", recruiterRouter)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.listen(port, () => {
-    console.log(`your server is  running on http://localhost:${port}`)
+    console.log(`🚀 Server running at http://localhost:${port}`);
+    console.log(`📚 Swagger docs at http://localhost:${port}/api-docs`);
 })
